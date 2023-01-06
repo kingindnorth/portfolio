06ffa4d0080d4f8e13257f978244ef9eab03a6c1
prajjawal tiwari <kingindnorth.0311@gmail.com>
2023-01-06 15:54:30 +0530

    static files added

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express")
+
+require("dotenv").config()
+
+const PORT = process.env.PORT || 3000
+
+const app = express()
+
+//serve static files
+app.use(express.static("public"))
+
+app.listen(PORT,()=>{
+    console.log("server started");
+})
