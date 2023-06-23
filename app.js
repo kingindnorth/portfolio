d58243a95d4570e15fd251f2b46fbbc698784d4c
prajjawal tiwari <kingindnorth.0311@gmail.com>
2023-06-23 13:09:29 +0530

    email notification logic added

--- app.js
+++ app.js
@@ -1,10 +1,73 @@
 const express = require("express")
 
 require("dotenv").config()
+const nodemailer = require("nodemailer")
 
 const PORT = process.env.PORT || 3000
 
 const app = express()
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+
+const transporter = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    auth: {
+      user: process.env.EMAIL_USERNAME,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+  }); 
+
+app.use("/submit", sendEmail)
+
+// Function to send the email
+async function sendEmail(req,res) {
+  const {firstName, lastName, email, subject, text} = req.body
+  const mailOptions = {
+    from: email,
+    to: process.env.EMAIL_USERNAME,
+    subject: subject,
+    text: JSON.stringify(text),
+  };
+
+  try {
+    const info = await transporter.sendMail(mailOptions);
+    console.log('Email sent:', info.response);
+    //send mail to sender that response recorded.
+    const info2 = await transporter.sendMail({
+        from: process.env.EMAIL_USERNAME,
+        to: email,
+        subject:"THANKYOU FOR REACHING OUT!",
+        text: `Dear ${firstName} ${lastName},
+
+        I wanted to take a moment to express my sincere gratitude for reaching out to me. Your message has truly brightened my day, and I appreciate your time and effort in connecting with me.
+        
+        Your interest, questions, or feedback mean a great deal to me as they help me grow and improve. I am grateful for the opportunity to engage with you and learn more about your experiences and expectations.
+        
+        Please know that your inquiry is important to me, and I am committed to addressing it promptly and thoroughly. I am already working diligently to provide you with the assistance or information you require. Your trust in me is valued, and I aim to exceed your expectations.
+        
+        Once again, thank you for reaching out to me. I value your support and look forward to serving you in the best possible manner. If there's anything else I can assist you with, please don't hesitate to let me know.
+        
+        Warmest regards,
+        
+        Prajjawal Tiwari`
+
+    })
+    console.log("email sent to user ", info2.response)
+    res.redirect("/")
+  } catch (error) {
+    console.error('Error sending email:', error);
+  }
+}
+
+// // Example usage
+// const formSubmission = {
+//   email: 'prajjawal.tiwari@niveussolutions.com',
+//   subject: 'trial mail',
+//   text: 'Hello, I have a question.',
+// };
+
+// sendEmail(formSubmission);
 
 //serve static files
 app.use(express.static("public"))
